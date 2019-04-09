6d0afee1fa3ed07c80a55e22b95d02c85dfc2b29
CJ Enright <cenrigh@purdue.edu>
2019-04-09 18:58:02 -0400

    Cleans up JWT configuring

--- internal/http/api/handler.go
+++ internal/http/api/handler.go
@@ -56,6 +56,17 @@ func NewHandler(us models.UserService) *Handler {
 
 // postSignup tries to sign up a user.
 func (h *Handler) postSignup() http.HandlerFunc {
+	jwtIssuer, ok := os.LookupEnv("JWT_COOKIE_NAME")
+	if !ok {
+		log.Fatalf("environment variable not set: %v", "JWT_ISSUER")
+	}
+
+	jwtSigningKeyString, ok := os.LookupEnv("JWT_SIGNING_KEY")
+	if !ok {
+		log.Fatalf("environment variable not set: %v", "JWT_SIGNING_KEY")
+	}
+	jwtSigningKey := []byte(jwtSigningKeyString)
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO check if login is valid (i.e. account exists), if so log them in
 
@@ -76,7 +87,7 @@ func (h *Handler) postSignup() http.HandlerFunc {
 		}
 		u.ID = id
 
-		jwt, err := u.GetJWT()
+		jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
 		if err != nil {
 			// TODO error handling
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -95,6 +106,17 @@ func (h *Handler) postSignup() http.HandlerFunc {
 
 // postLogin tries to log in a user.
 func (h *Handler) postLogin() http.HandlerFunc {
+	jwtIssuer, ok := os.LookupEnv("JWT_COOKIE_NAME")
+	if !ok {
+		log.Fatalf("environment variable not set: %v", "JWT_ISSUER")
+	}
+
+	jwtSigningKeyString, ok := os.LookupEnv("JWT_SIGNING_KEY")
+	if !ok {
+		log.Fatalf("environment variable not set: %v", "JWT_SIGNING_KEY")
+	}
+	jwtSigningKey := []byte(jwtSigningKeyString)
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Convert JSON user details in request to a user struct
 		var u models.User
@@ -113,7 +135,7 @@ func (h *Handler) postLogin() http.HandlerFunc {
 			return
 		}
 
-		jwt, err := u.GetJWT()
+		jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
 		if err != nil {
 			// TODO error handling
 			http.Error(w, err.Error(), http.StatusInternalServerError)
