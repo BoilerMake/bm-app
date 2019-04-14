de2d2941a34971849c9ab37d4b56ef7f003a435f
MaxLin <maxylin@gmail.com>
2019-04-14 02:34:13 -0400

    Change timestamptz (time zone)

--- migrations/20190407143451_add_password_reset_token_table.sql
+++ migrations/20190407143451_add_password_reset_token_table.sql
@@ -1,9 +1,10 @@
 -- +goose Up
 CREATE TABLE password_reset_tokens (
   id SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  uid INTEGER UNIQUE NOT NULL,
-  token TEXT NOT NULL,
-  valid_until TIMESTAMP default current_timestamp + interval '1 hour',
+  uid INTEGER NOT NULL,
+  tokenID TEXT UNIQUE NOT NULL,
+  hashedToken TEXT UNIQUE NOT NULL,
+  created_at TIMESTAMPTZ default current_timestamp,
   FOREIGN KEY (uid) REFERENCES users (id)
 );
 -- +goose Down
