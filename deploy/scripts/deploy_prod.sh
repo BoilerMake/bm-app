aaea24ccae1ef6baceb05887a2ddfaedc7a58639
CJ Enright <cenrigh@purdue.edu>
2019-05-27 21:48:40 -0500

    Fix config for traefik in prod

--- deploy/scripts/deploy_prod.sh
+++ deploy/scripts/deploy_prod.sh
@@ -5,6 +5,10 @@ cd ~/backend
 # Update our repo
 git pull
 
+# Make and set permissions on acme.json for let's encrypt
+sudo mkdir -p /opt/traefik
+sudo touch /opt/traefik/acme.json && sudo chmod 600 /opt/traefik/acme.json
+
 # This will rebuild our serivces *then* replace the current ones with them.
 # Avoiding downtime is cool
-docker-compose -f deploy/docker-compose.base.yml -f deploy/docker-compose.prod.yml up -d --build
+sudo docker-compose -f deploy/docker-compose.base.yml -f deploy/docker-compose.prod.yml up -d --build
