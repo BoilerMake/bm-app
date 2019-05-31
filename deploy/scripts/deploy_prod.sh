38bdcf4943b18432a8f3e01ab1f0e509f07da173
CJ Enright <cenrigh@purdue.edu>
2019-05-31 18:38:17 -0500

    small prod touchups

--- deploy/scripts/deploy_prod.sh
+++ deploy/scripts/deploy_prod.sh
@@ -5,10 +5,13 @@ cd ~/backend
 # Update our repo
 git pull
 
+# Make sure pgdata exists
+sudo mkdir -p /var/lib/pgsql/data
+
 # Make and set permissions on acme.json for let's encrypt
 sudo mkdir -p /opt/traefik
 sudo touch /opt/traefik/acme.json && sudo chmod 600 /opt/traefik/acme.json
 
 # This will rebuild our serivces *then* replace the current ones with them.
 # Avoiding downtime is cool
-sudo docker-compose -f deploy/docker-compose.base.yml -f deploy/docker-compose.prod.yml up -d --build
+sudo docker-compose -f ~/backend/deploy/docker-compose.base.yml -f ~/backend/deploy/docker-compose.prod.yml up -d --build
