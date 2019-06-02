501a7a757fdc3633b01e573f5ba7dfb99cb455b3
CJ Enright <cenrigh@purdue.edu>
2019-06-01 21:24:33 -0500

    Adds account template

--- internal/http/web/handler.go
+++ internal/http/web/handler.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +13,7 @@ import (
 	"github.com/BoilerMake/new-backend/internal/http/middleware"
 	"github.com/BoilerMake/new-backend/internal/models"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 )
 
@@ -65,6 +68,8 @@ func NewHandler(us models.UserService) *Handler {
 	r.Get("/login", h.getLogin())
 	r.Post("/login", h.postLogin())
 
+	r.Get("/account", h.getAccount())
+
 	h.Mux = r
 	return &h
 }
@@ -111,7 +116,6 @@ func (h *Handler) getRoot() http.HandlerFunc {
 }
 
 func (h *Handler) getSignup() http.HandlerFunc {
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		h.templates.ExecuteTemplate(w, "signup", nil)
 	}
@@ -190,6 +194,33 @@ func (h *Handler) postLogin() http.HandlerFunc {
 	}
 }
 
+func (h *Handler) getAccount() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO redirect to login if they're not logged in
+		claims, err := getClaimsFromCtx(r.Context())
+		if err != nil {
+			// TODO error handling
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+		}
+
+		u, err := h.UserService.GetByEmail(claims["email"].(string))
+		if err != nil {
+			// TODO error handling
+			// This can fail either because the DB is messed up or nothing is found
+			// So be sure to deal with that
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+		}
+
+		data := map[string]interface{}{
+			"Email":     u.Email,
+			"FirstName": u.FirstName,
+			"LastName":  u.LastName,
+		}
+
+		h.templates.ExecuteTemplate(w, "account", data)
+	}
+}
+
 // mustGetJWTConfig tries to get JWT configuration variables from the
 // environment. It will panic if those variables are not set.
 func mustGetJWTConfig() (string, []byte) {
@@ -205,4 +236,26 @@ func mustGetJWTConfig() (string, []byte) {
 	jwtSigningKey := []byte(jwtSigningKeyString)
 
 	return jwtIssuer, jwtSigningKey
+}
+
+// getClaimsFromCtx returns the claims of a Context's JWT or an error.
+func getClaimsFromCtx(ctx context.Context) (claims jwt.MapClaims, err error) {
+	// Always make sure the error field is nil
+	err, _ = ctx.Value(middleware.JWTErrorCtxKey).(error)
+	if err != nil {
+		return nil, err
+	}
+
+	// Make sure the token is not nil
+	token, ok := ctx.Value(middleware.JWTCtxKey).(*jwt.Token)
+	if !ok {
+		return nil, fmt.Errorf("missing jwt in context")
+	}
+
+	claims = token.Claims.(jwt.MapClaims)
+	if err = claims.Valid(); err != nil {
+		return nil, err
+	}
+
+	return claims, err
 }
