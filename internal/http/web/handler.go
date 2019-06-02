9bfa82a9ba3013abd51217b28c87b9b1a0727308
CJ Enright <cenrigh@purdue.edu>
2019-06-02 14:20:37 -0500

    Move template handling to its own file

--- internal/http/web/handler.go
+++ internal/http/web/handler.go
@@ -6,8 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"text/template"
 
 	"github.com/BoilerMake/new-backend/internal/http/middleware"
@@ -72,41 +70,6 @@ func NewHandler(us models.UserService) *Handler {
 
 	h.Mux = r
 	return &h
-}
-
-// Loads templates (files ending in .tmpl) from web/templates/ and all its
-// subdirectories.
-func (h *Handler) loadTemplates() (err error) {
-	h.templates = template.New("")
-
-	err = filepath.Walk("web/templates/", func(path string, _ os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if strings.HasSuffix(path, ".tmpl") {
-			template.Must(h.templates.ParseFiles(path))
-		}
-
-		return nil
-	})
-
-	return err
-}
-
-// reloadTemplates is a middleware that calls loadTemplates on every request.
-// It's useful to live reload templates in development but shouldn't be used in
-// production.
-func (h *Handler) reloadTemplates(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := h.loadTemplates()
-		if err != nil {
-			log.Fatalf("failed to reload templates: %s", err)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
 
 func (h *Handler) getRoot() http.HandlerFunc {
