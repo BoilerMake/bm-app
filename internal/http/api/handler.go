246c868eec3ec2f68a8b69bf47b3c0ff9641c910
Viraat Das <viraat.laldas@gmail.com>
2019-06-04 00:20:33 -0400

    code compiles; testing needs to be done

--- internal/http/api/handler.go
+++ internal/http/api/handler.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/BoilerMake/new-backend/internal/http/middleware"
 	"github.com/BoilerMake/new-backend/internal/models"
+	"github.com/dgrijalva/jwt-go"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	// jwt "github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
+
+	"github.com/gorilla/sessions"
 )
 
 var (
-	jwtCookie string // Name for the JWT's cookie.  TODO Better name?
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
 )
 
 // A Handler will route requests to their appropriate HandlerFunc.
@@ -28,17 +31,30 @@ type Handler struct {
 	UserService models.UserService
 }
 
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Check if user is authenticated
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	// Print secret message
+	fmt.Fprintln(w, "The cake is a lie!")
+}
+
 // NewHandler creates a handler for API requests.
 func NewHandler(us models.UserService) *Handler {
 	h := Handler{UserService: us}
 	r := chi.NewRouter()
 
 	// TODO See cmd/server/main.go for more about config. This doesn't seem ideal.
-	var ok bool
-	jwtCookie, ok = os.LookupEnv("JWT_COOKIE_NAME")
-	if !ok {
-		log.Fatalf("environment variable not set: %v", "JWT_COOKIE_NAME")
-	}
+	// var ok bool
+	// jwtCookie, ok = os.LookupEnv("JWT_COOKIE_NAME")
+	// if !ok {
+	// 	log.Fatalf("environment variable not set: %v", "JWT_COOKIE_NAME")
+	// }
 
 	r.Use(middleware.SetContentTypeJSON) // All responses from here will be JSON
 	r.Use(middleware.WithJWT)
@@ -56,11 +72,11 @@ func NewHandler(us models.UserService) *Handler {
 
 // postSignup tries to sign up a user.
 func (h *Handler) postSignup() http.HandlerFunc {
-	jwtIssuer, jwtSigningKey := mustGetJWTConfig()
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO check if login is valid (i.e. account exists), if so log them in
+		session, _ := store.Get(r, "cookie-name")
 
+		// TODO check if login is valid (i.e. account exists), if so log them in
 		var u models.User
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&u)
@@ -78,28 +94,33 @@ func (h *Handler) postSignup() http.HandlerFunc {
 		}
 		u.ID = id
 
-		jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
-		if err != nil {
-			// TODO error handling
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		// jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
+		// if err != nil {
+		// 	// TODO error handling
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// }
 
 		// TODO right now this is only valid on the domain it's sent from, if we do
 		// subdomains (seems likely) then we'll need to change that.
-		http.SetCookie(w, &http.Cookie{
-			Name:       jwtCookie,
-			Value:      jwt,
-			Path:       "/",
-			RawExpires: "0",
-		})
+		// http.SetCookie(w, &http.Cookie{
+		// 	Name:       jwtCookie,
+		// 	Value:      jwt,
+		// 	Path:       "/",
+		// 	RawExpires: "0",
+		// })
+
+		// Set user as authenticated
+		session.Values["authenticated"] = true
+		session.Save(r, w)
 	}
 }
 
 // postLogin tries to log in a user.
 func (h *Handler) postLogin() http.HandlerFunc {
-	jwtIssuer, jwtSigningKey := mustGetJWTConfig()
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "cookie-name")
+
 		// Convert JSON user details in request to a user struct
 		var u models.User
 		decoder := json.NewDecoder(r.Body)
@@ -117,20 +138,24 @@ func (h *Handler) postLogin() http.HandlerFunc {
 			return
 		}
 
-		jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
-		if err != nil {
-			// TODO error handling
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		// jwt, err := u.GetJWT(jwtIssuer, jwtSigningKey)
+		// if err != nil {
+		// 	// TODO error handling
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// }
 
 		// TODO Right now this is only valid on the domain it's sent from, if we do
 		// subdomains (seems likely) then we'll need to change that.
-		http.SetCookie(w, &http.Cookie{
-			Name:       jwtCookie,
-			Value:      jwt,
-			Path:       "/",
-			RawExpires: "0",
-		})
+		// http.SetCookie(w, &http.Cookie{
+		// 	Name:       jwtCookie,
+		// 	Value:      jwt,
+		// 	Path:       "/",
+		// 	RawExpires: "0",
+		// })
+
+		// Set user as authenticated
+		session.Values["authenticated"] = true
+		session.Save(r, w)
 	}
 }
 
@@ -164,20 +189,20 @@ func (h *Handler) getSelf() http.HandlerFunc {
 
 // mustGetJWTConfig tries to get JWT configuration variables from the
 // environment. It will panic if those variables are not set.
-func mustGetJWTConfig() (string, []byte) {
-	jwtIssuer, ok := os.LookupEnv("JWT_COOKIE_NAME")
-	if !ok {
-		log.Fatalf("environment variable not set: %v", "JWT_ISSUER")
-	}
-
-	jwtSigningKeyString, ok := os.LookupEnv("JWT_SIGNING_KEY")
-	if !ok {
-		log.Fatalf("environment variable not set: %v", "JWT_SIGNING_KEY")
-	}
-	jwtSigningKey := []byte(jwtSigningKeyString)
-
-	return jwtIssuer, jwtSigningKey
-}
+// func mustGetJWTConfig() (string, []byte) {
+// 	jwtIssuer, ok := os.LookupEnv("JWT_COOKIE_NAME")
+// 	if !ok {
+// 		log.Fatalf("environment variable not set: %v", "JWT_ISSUER")
+// 	}
+//
+// 	jwtSigningKeyString, ok := os.LookupEnv("JWT_SIGNING_KEY")
+// 	if !ok {
+// 		log.Fatalf("environment variable not set: %v", "JWT_SIGNING_KEY")
+// 	}
+// 	jwtSigningKey := []byte(jwtSigningKeyString)
+//
+// 	return jwtIssuer, jwtSigningKey
+// }
 
 // getClaimsFromCtx returns the claims of a Context's JWT or an error.
 func getClaimsFromCtx(ctx context.Context) (claims jwt.MapClaims, err error) {
