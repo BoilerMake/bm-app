f562170d20c4014302276fca3293a25f367917ae
CJ Enright <cenrigh@purdue.edu>
2019-08-11 12:06:02 -0500

    Update applicaiton to use bulma

--- web/src/scripts/index.js
+++ web/src/scripts/index.js
@@ -1,22 +1,46 @@
-// This is where scripts go!
 document.addEventListener('DOMContentLoaded', () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+	// Listen for clicks on hamburger button
+	const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+	// Check if there are any navbar burgers
+	if (navbarBurgers.length > 0) {
+		// Add a click event on each of them
+		navbarBurgers.forEach( el => {
+			el.addEventListener('click', () => {
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
-  }
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+			});
+		});
+	}
+
+
+	// Update file upload labels when file changes
+	const fileUploads = document.querySelectorAll('.file-input');
+	// Check if there are any file uploads
+	if (fileUploads.length > 0) {
+		// Add an on change function to each
+		fileUploads.forEach( el => {
+			el.onchange = function() {
+				// Make sure a file was selected
+				if (el.files.length > 0) {
+					var sib = el.nextElementSibling;
+
+					// Look for next sibling that has class
+					while (sib) {
+						if (sib.matches('.file-name')) {
+							sib.textContent = el.files[0].name;
+						}
+
+						sib = sib.nextElementSibling
+					}
+				}
+			}
+		});
+	};
 });
