abb7dc0dec39edffa741912c933f54bae62b7a91
MaxLin <maxylin@gmail.com>
2020-01-08 17:27:24 -0800

    Add announcement endpoints: get,post,delete

---
+++ internal/http/web/announcement.go
@@ -0,0 +1,90 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/BoilerMake/bm-app/internal/models"
+)
+
+type announcementMessage struct {
+	Message string `json:"message"`
+}
+
+type idMessage struct {
+	ID int `json:"id"`
+}
+
+func (h *Handler) postAnnouncement() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var am announcementMessage
+		err := json.NewDecoder(r.Body).Decode(&am)
+		if err != nil {
+			h.Error(w, r, err, "")
+			return
+		}
+
+		// Check if message is empty or not
+		if am.Message == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			h.Error(w, r, models.ErrAnnouncementMessageEmpty, "")
+			return
+		}
+
+		err = h.AnnouncementService.Create(am.Message)
+
+		if err != nil {
+			h.Error(w, r, err, "")
+			return
+		}
+
+		// Indicate created resource
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func (h *Handler) getAnnouncement() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		currentAnnouncement, err := h.AnnouncementService.GetCurrent()
+		if err == models.ErrNoAnnouncements {
+			w.WriteHeader(http.StatusNoContent)
+			h.Error(w, r, err, "")
+			return
+		}
+
+		if err != nil {
+			h.Error(w, r, err, "")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(currentAnnouncement)
+	}
+}
+
+func (h *Handler) deleteAnnouncement() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var m idMessage
+		err := json.NewDecoder(r.Body).Decode(&m)
+		if err != nil {
+			h.Error(w, r, err, "")
+			return
+		}
+
+		// Check if id is empty
+		if m.ID == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			h.Error(w, r, models.ErrAnnouncementIDEmpty, "")
+			return
+		}
+
+		err = h.AnnouncementService.DeleteByID(m.ID)
+		if err != nil {
+			h.Error(w, r, err, "")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
